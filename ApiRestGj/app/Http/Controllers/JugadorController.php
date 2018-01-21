4f7f3f09308553c8f5bffe7fa2aa28985ea50122
Gorky Suquinagua <gsuquinagua@hotmail.es>
2018-01-21 00:09:36 -0500

    COMMIT
    Rutas de api y controladores

---
+++ ApiRestGj/app/Http/Controllers/JugadorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jugador;
+use Illuminate\Http\Request;
+
+class JugadorController extends Controller
+{
+    //Obtener jugadores
+    public function getJugadores()
+    {
+        return response()->json(Jugador::all());
+    }
+
+    //Obtener jugador por nombre
+    public function getJugador($nick)
+    {
+        $jugador = Jugador::where('nickname', $nick)->first();
+        return response()->json($jugador);
+    }
+
+    
+
+}
--- ApiRestGj/routes/web.php
+++ ApiRestGj/routes/web.php
@@ -14,3 +14,20 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    //Obtener los jugadores que estan en el juego
+    $router->get('jugadores', ['uses' => 'JugadorController@getJugadores']);
+
+    //Obtener el jugador que esta pidiendo por el nickname
+    $router->get('jugador/{nick}', ['uses' => 'JugadorController@getJugador']);
+
+    //Agregar un nuevo jugador
+    $router->post('jugador', ['uses' => 'JugadorController@nuevoJugador']);
+
+    //Registramos el seguimiento
+    $router->post('seguimiento', ['uses' => 'SeguimientoController@nuevoSeguimiento']);
+
+    //Registramos el puntaje del jugador
+    $router->put('jugador/{id}/{estrellas}/{copas}', ['uses' => 'JugadorController@registrarPuntaje']);
+});
