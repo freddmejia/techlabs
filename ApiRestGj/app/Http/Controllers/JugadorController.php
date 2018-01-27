e7563d0a21d416638021309d2397e961f80e9cde
Gorky Suquinagua <gsuquinagua@hotmail.es>
2018-01-27 13:39:48 -0500

    COMMIT
    NUEVO JUGADOR

--- ApiRestGj/app/Http/Controllers/JugadorController.php
+++ ApiRestGj/app/Http/Controllers/JugadorController.php
@@ -23,16 +23,9 @@ class JugadorController extends Controller
     //Se crea un nuevo jugador
     public function nuevoJugador($nick,$genre)
     {
-<<<<<<< HEAD
-        $nuevoJugador = Jugador::create(
-            ['nickname' => $nick,
-            'copas' => $copas,
-            'estrellas' => $estrellas,
-            'estado' => true
-=======
         //verificar que no exista el jugador
-        $player=Jugador::where('nickname','=',$nick)
-                    ->where('genero','=',$genre)
+        $player=Jugador::where('nickname',$nick)
+                    ->where('genero',$genre)
                     ->get()
                     ->first();
         if(!empty($player))
@@ -44,12 +37,12 @@ class JugadorController extends Controller
             $nuevoJugador = new Jugador();
             $nuevoJugador->nickname=$nick;
             $nuevoJugador->genero=$genre;
+            $nuevoJugador->estado = 1;
             $nuevoJugador->save();  
             return $data["data"]="null";  
         }
        /* $nuevoJugador = Jugador::create(
             ['nickname' => $nick, 
->>>>>>> 32864ec02151147892b34b20bdfd71d559dd88b5
             ]
         );*/
 
